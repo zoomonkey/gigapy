12a019c4200b4f4d165f2973fd70f2581f47f98f
zoomonkey <codingisforyou@outlook.com>
2019-08-18 09:26:43 -0500

    security

--- gigaout.py
+++ gigaout.py
@@ -1,11 +1,18 @@
 import boto3
 import datetime
+from configparser import ConfigParser
+
+parser = ConfigParser()
+parser.read('config.ini')
+
+mk = str(parser.get('myconfig', 'key'))
+ms = str(parser.get('myconfig', 'secret'))
 
 now = datetime.datetime.now()
 
 client = boto3.client(
     's3',
-    aws_access_key_id='AKIA6BX4WBMPBRX5OTIF',
-    aws_secret_access_key='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'
+    aws_access_key_id=mk,
+    aws_secret_access_key=ms
 )
 client.upload_file('output.log','gigaoutput','gigaoutput' + str(now.month) + '-' + str(now.day))
